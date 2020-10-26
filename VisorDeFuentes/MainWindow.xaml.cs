4ce13af96be6e35b38c7153cf49dc81637ca7e8c
mauriciopinzon-t <19470204@itcampeche.edu.mx>
2020-10-26 13:15:24 -0600

    Se agregaron los manejadores de eventos

--- VisorDeFuentes/MainWindow.xaml.cs
+++ VisorDeFuentes/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace VisorDeFuentes
         {
             InitializeComponent();
         }
+
+        private void fuenteComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fuenteComboBox.SelectedItem != null)
+            {
+                string fuente = (string)(fuenteComboBox.SelectedItem as ComboBoxItem).Content;
+                leyendaTextBlock.FontFamily = new FontFamily(familyName: fuente);
+            }
+
+        }
+        private void cursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            leyendaTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void cursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            leyendaTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void negritasCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            leyendaTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+        private void negritasCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            leyendaTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+        private void tamanioRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (smallRadioButton.IsChecked == true)
+            {
+                leyendaTextBlock.FontSize = 12;
+            }
+            else if (mediumRadioButton.IsChecked == true)
+            {
+                leyendaTextBlock.FontSize = 18;
+            }
+            else
+            {
+                leyendaTextBlock.FontSize = 24;
+            }
+        }
     }
+    
 }
